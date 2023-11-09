6575425e45054e5e63c19f008abd900fd678b16b
BlackBeard88688 <122628733+BlackBeard88688@users.noreply.github.com>
2023-11-09 14:54:42 +0530

    dist removed

--- dist/routes/v1/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const brand_js_1 = require("./brands/brand.js");
-const v1Routes = (0, express_1.Router)();
-v1Routes.use("/v1/brands", brand_js_1.brandRoutes);
-exports.default = v1Routes;
-//# sourceMappingURL=index.js.map
